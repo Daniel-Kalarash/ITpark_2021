f71607efa5f0e3dbc836214cda20ae32326f9f6b
Daniel-Kalarash <danilka-spam@mail.ru>
2021-11-05 17:12:14 +0300

    исправление грамматической ошибки, второе задание

---
+++ src/main/java/SecondProgram.java
@@ -0,0 +1,14 @@
+public class SecondProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!");
+
+        long summa = calculate(6,12);
+        System.out.println(summa);
+        }
+
+    public static long calculate(int arg1, int arg2) {
+        return (long) arg1*arg2+arg2/arg1;
+    }
+}
+
