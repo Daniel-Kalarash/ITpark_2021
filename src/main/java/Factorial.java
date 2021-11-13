860a9bce1a58d7465fa792da3c315bdcf9bda7e4
Daniel-Kalarash <danilka-spam@mail.ru>
2021-11-13 22:30:17 +0300

    Решение факториала

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -4,7 +4,7 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Расчет факториала. Введите целое число");
+        System.out.println("Расчет факториала. Введите целое число от 1 до 25");
         long n = scanner.nextInt();
 
         long fact = 1; int i = 1;
