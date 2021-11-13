71ca889fbb38bee09e64ae88b700bb1dbbf9079e
Daniel-Kalarash <danilka-spam@mail.ru>
2021-11-13 11:49:22 +0300

    Решение квадратного уравнения

--- src/main/java/SecondProgram.java
+++ src/main/java/SecondProgram.java
@@ -3,12 +3,11 @@ public class SecondProgram {
     public static void main(String[] args) {
         System.out.println("Привет, мир!");
 
-        long summa = calculate(4,12);
+        long summa = calculate(4, 12);
         System.out.println(summa);
-        }
+    }
 
     public static long calculate(int arg1, int arg2) {
-        return (long) arg1*arg2+arg2/arg1;
+        return (long) arg1 * arg2 + arg2 / arg1;
     }
 }
-
